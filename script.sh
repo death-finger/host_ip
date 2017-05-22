3840c6d710a0418ca09ab2ca230094a6ce4832d9
root <death_finger@sina.com>
2017-05-22 11:30:02 +0800

    Updated

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
+
+cd /root/zabbix_ip
 sshpass -p zabbix ssh zabbix@192.168.1.1 -p 10022 "put [ip address get [/ip address find interface=GWBN] address]" > ./address.txt
 
 git commit -a -m "Updated"
