487af72817df30cd7934ea8207199aaaab9c9740
root <death_finger@sina.com>
2017-10-14 18:01:52 +0800

    Updated

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
 
 cd /root/zabbix_ip
-sshpass -p zabbix ssh zabbix@192.168.1.1 -p 10022 "put [ip address get [/ip address find interface=GWBN] address]" > ./address.txt
+w3m -dump http://submit.apnic.net/templates/yourip.html | grep -P -o '(\d+\.){3}\d+' > ~/address/address.txt
 
 git commit -a -m "Updated"
 git push origin master
