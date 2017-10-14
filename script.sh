833f7f1430e0c81a7f79c55e8bd7e32b4be56694
root <death_finger@sina.com>
2017-10-14 18:05:32 +0800

    Updated

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 
 
 cd /root/zabbix_ip
-w3m -dump http://submit.apnic.net/templates/yourip.html | grep -P -o '(\d+\.){3}\d+' > ~/address/address.txt
+w3m -dump http://submit.apnic.net/templates/yourip.html | grep -P -o '(\d+\.){3}\d+' > ~/zabbix_ip/address.txt
 
 git commit -a -m "Updated"
 git push origin master
